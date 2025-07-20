5e42fd79bd546fa07fc59252618a5bbf53268b8e
Gustavo Cornejo <93890321+gcornejov@users.noreply.github.com>
2025-07-20 01:39:00 -0400

    Add environment variable for GCloud Credentials Volume (#4)
    * Add p10k feature in devcontainer
    
    * Update .zshrc file
    
    * Add CHANGELOG in p10k feature

--- features/test/zsh-p10k/test.sh
+++ features/test/zsh-p10k/test.sh
@@ -12,10 +12,6 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-# check "validate powerlevel10k theme files" $([ -d "$ZSH_CUSTOM/themes/powerlevel10k" ] && [ ! -z "$( ls -A "$ZSH_CUSTOM/themes/powerlevel10k" )" ]) || exit 1
-# check "validate powerlevel10k config file" $([ -f "$HOME/.p10k.zsh" ]) || exit 1
-# check "validate powerlevel10k theme setup" grep -q "powerlevel10k/powerlevel10k" $HOME/.zshrc
-
 check "validate powerlevel10k theme files" [ -d "$ZSH_CUSTOM/themes/powerlevel10k" ] && [ ! -z "$( ls -A "$ZSH_CUSTOM/themes/powerlevel10k" )" ] || exit 1
 check "validate powerlevel10k config file" [ -f "$HOME/.p10k.zsh" ] || exit 1
 check "validate powerlevel10k theme setup" grep -q "powerlevel10k/powerlevel10k" $HOME/.zshrc
